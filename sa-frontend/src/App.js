3bc5b0968dde1e93d68729838ed209a0feae678a
JLohnson1999 <chihsial@andrew.cmu.edu>
2025-02-13 08:05:55 +0000

    Initial commit - Added Kubernetes deployment files

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch("http://34.42.45.167:8080/sentiment", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
